5c1bc62c5ac86733885036b437fb1e673c521a34
Phong <a1873825@adelaide.edu.au>
2025-08-28 13:18:36 +0930

    fix seat layout

--- client/src/assets/dummy.js
+++ client/src/assets/dummy.js
@@ -32,7 +32,7 @@ export const destinations = [
         ],
         departureTimes: ["09:00", "10:00", "11:00"],
         price: 25,
-        seatLayout: ["A1","A2","A3","A4","B1","B2","B3","B4"]
+        seatLayout: ["A1","A2","A3","A4","B1","B2","B3","B4","C1","C2","C3","C4","D1","D2","D3","D4","E1","E2","E3","E4","F1","F2","F3","F4","G1","G2","G3","G4","H1","H2","H3","H4"]
       }
     ]
   },
@@ -51,7 +51,7 @@ export const destinations = [
         ],
         departureTimes: ["08:30", "12:30", "16:30"],
         price: 35,
-        seatLayout: ["A1","A2","A3","A4","B1","B2","B3","B4"]
+        seatLayout: ["A1","A2","A3","A4","B1","B2","B3","B4","C1","C2","C3","C4","D1","D2","D3","D4","E1","E2","E3","E4","F1","F2","F3","F4","G1","G2","G3","G4","H1","H2","H3","H4"]
       }
     ]
   }
