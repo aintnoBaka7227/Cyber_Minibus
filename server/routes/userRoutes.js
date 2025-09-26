6732ee7db845fc4e2da4b51162e4c9f27bb4ea53
Vasu <vastavik987@gmail.com>
2025-09-26 23:53:26 +0930

    defined all endpoints

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,7 +1,9 @@
-// import express from "express";
-// import { getUserBookings } from "../controllers/userController.js";
+import express from "express";
+import { getUserBookings, getUserProfile, updateUserProfile } from "../controllers/userController.js";
+import { requireAuth } from "../middleware/auth.js";
 
-// export const userRouter = express.Router();
+export const userRouter = express.Router();
 
-// userRouter.get("/bookings", getUserBookings);
-// userRouter.get("/profile", getUserProfile);
+userRouter.get("/bookings", requireAuth ,getUserBookings);    // User can see all the bookings that they have made
+userRouter.get("/profile", requireAuth, getUserProfile);      // User can get all of their profile details
+userRouter.post("/update-profile", requireAuth ,updateUserProfile);   // User can update their profile details
