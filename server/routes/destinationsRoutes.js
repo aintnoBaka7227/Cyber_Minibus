04ebaf53b646d7964cc5e2ff127b82a61ebd8a63
Vasu <vastavik987@gmail.com>
2025-10-09 18:48:16 +1030

    Successfully defined all the required routes

--- server/routes/destinationsRoutes.js
+++ server/routes/destinationsRoutes.js
@@ -8,10 +8,8 @@ import {
 
 import { requireAdmin } from "../middleware/auth.js";
 
-const destinationRouter = express.Router();
+export const destinationRouter = express.Router();
 
 destinationRouter.get("/all-destinations", getDestinations);      // All the destinations can be viewed on the web page without logging in
 destinationRouter.get("/get-destination-details/:destinationId", getDestination);   // When user clicks a particular destination, the details of that destination will be displayed
 destinationRouter.post("/add-destination", requireAdmin, addDestination);       // This endpoint is called when Admin adds a destination to the list of available destinations
-
-export default destinationRouter;
