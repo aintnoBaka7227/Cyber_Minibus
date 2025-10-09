7151be49cb985672f83a84383d3eb928d7786e99
Vasu <vastavik987@gmail.com>
2025-10-09 18:48:16 +1030

    Successfully defined all the required routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,6 @@ import { requireAuth } from "../middleware/auth.js";
 
 export const userRouter = express.Router();
 
-userRouter.get("/bookings", requireAuth ,getUserBookings);    // User can see all the bookings that they have made
-userRouter.get("/profile", requireAuth, getUserProfile);      // User can get all of their profile details
-userRouter.post("/update-profile", requireAuth ,updateUserProfile);   // User can update their profile details
+userRouter.get("/bookings/:userId", requireAuth ,getUserBookings);    // User can see all the bookings that they have made
+userRouter.get("/profile/:userId", requireAuth, getUserProfile);      // User can get all of their profile details
+userRouter.post("/update-profile/:userId", requireAuth ,updateUserProfile);   // User can update their profile details
